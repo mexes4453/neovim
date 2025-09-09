5cb40e98f7f0bc0f5bb2620424b722c40f2c2a11
cudoh <cudoh@student.42wolfsburg.de>
2025-09-09 23:19:41 +0200

    second commit

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,11 @@
+--key leader: This like ctrl in "ctrl+ p" for printing
+--            In this case, space serves the purpose of
+--            ctrl as the lead key.
+vim.g.mapleader = " "
+
+-- keymap : args ( <mode>, <keybind>, <cmd> )
+--               ( normal, 'spacebar + cd', :Ex )
+-- In normal mode, pressing "spacebar + cd' performs the same action
+-- as ":Ex" which opens the directory while working on a file in nvim.
+vim.keymap.set("n", "<leader>cd", ":Ex<CR>")
+
